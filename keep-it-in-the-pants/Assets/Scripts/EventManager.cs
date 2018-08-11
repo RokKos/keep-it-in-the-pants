73378a2e4186258265e60ae367282e81e14968ce
Andrej <andrej.miscic@gmail.com>
2018-08-11 09:50:58 +0200

    EventManager & Touch implemented

---
+++ keep-it-in-the-pants/Assets/Scripts/EventManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour {
+
+    public static EventManager Instance = null;
+
+    public UnityEvent<Vector3> OnDirectionInputChanged;
+
+    private void Awake() {
+        if (!Instance) {
+            Instance = this;
+        }
+    }
+}
--- keep-it-in-the-pants/Assets/Scripts/TouchController.cs
+++ keep-it-in-the-pants/Assets/Scripts/TouchController.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class TouchController : MonoBehaviour {
 
-	// Use this for initialization
+    private Vector3 lastTouchPosition;
+
 	void Start () {
-		
+        lastTouchPosition = Vector3.zero;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetMouseButtonDown(0)) {
+            lastTouchPosition = Input.mousePosition;
+        } else if (Input.GetMouseButton(0)) {
+            Vector3 newTouchPosition = Input.mousePosition;
+            Vector3 inputDiff = newTouchPosition - lastTouchPosition;
+            lastTouchPosition = newTouchPosition;
+
+            EventManager.Instance.OnDirectionInputChanged.Invoke(inputDiff);
+        }
 	}
 }
