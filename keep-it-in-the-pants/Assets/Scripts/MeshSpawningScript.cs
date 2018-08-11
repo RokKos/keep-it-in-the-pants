2507ef9bf06bd7a07b2b7de4c6a5c2b71b4dd95e
Mark Marincek <mark.marincek@gmail.com>
2018-08-11 12:52:29 +0200

    Updated mesh generation script

--- keep-it-in-the-pants/Assets/Scripts/MeshSpawningScript.cs
+++ keep-it-in-the-pants/Assets/Scripts/MeshSpawningScript.cs
@@ -11,80 +11,82 @@ public class MeshSpawningScript : MonoBehaviour {
 
     private float piValue;
 
+    private List<Vector3> playerLocations = new List<Vector3>();
     private List<Vector3> meshVertiecesList = new List<Vector3>();
     private List<int> meshTriangles = new List<int>();
 
-	// Use this for initialization
-	void Start () {
-        if (this.numberOfVertices % 2 != 0) this.numberOfVertices += 1;
+    private MeshFilter snakeMeshFilter = null;
+    private MeshRenderer snakeMeshRenderer = null;
 
+    // Use this for initialization
+    void Start() {
+        snakeMeshFilter = gameObject.AddComponent<MeshFilter>();
+        snakeMeshRenderer = gameObject.AddComponent<MeshRenderer>();
+
+        EventManager.Instance.OnPlayerPositionChanged.AddListener(PositionChanged);
         piValue = Mathf.PI * 2 / this.numberOfVertices;
-        this.meshVertiecesList.Add(this.testPoints[0]);
-        this.GenerateNewVertices();
-        this.GenerateTriangles();
-        this.PrintList();
-        Debug.Log("Count " + this.meshTriangles.Count);
-        this.AddMeshToGO();
+        if (this.numberOfVertices % 2 != 0) this.numberOfVertices += 1;
 
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+    void PositionChanged(Vector3 pos) {
+        this.playerLocations.Add(pos);
+        this.GenerateNewVertices();
+        this.GenerateTriangles();
+        this.AddMeshToGO();
+    }
 
     void AddMeshToGO() {
-        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
-        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
- 
-        meshFilter.mesh = new Mesh();
-        meshFilter.mesh.name = "ThickBlackSnake";
-        meshFilter.mesh.vertices = this.meshVertiecesList.ToArray();
-        meshFilter.mesh.triangles = this.meshTriangles.ToArray();
-        meshFilter.mesh.RecalculateNormals();
+        snakeMeshFilter = gameObject.GetComponent<MeshFilter>();
+        snakeMeshRenderer = gameObject.GetComponent<MeshRenderer>();
 
-        meshRenderer.material = this.snakeMaterial;
+        snakeMeshFilter.mesh = new Mesh();
+        snakeMeshFilter.mesh.name = "ThickBlackSnake";
+        snakeMeshFilter.mesh.vertices = this.meshVertiecesList.ToArray();
+        snakeMeshFilter.mesh.triangles = this.meshTriangles.ToArray();
+        snakeMeshFilter.mesh.RecalculateNormals();
+
+        snakeMeshRenderer.material = this.snakeMaterial;
 
     }
 
     void PrintList() {
-        foreach(Vector3 e in this.meshVertiecesList) {
+        foreach (Vector3 e in this.meshVertiecesList) {
             Debug.Log(e);
         }
-        foreach(int e in this.meshTriangles) {
+        foreach (int e in this.meshTriangles) {
             Debug.Log(e);
         }
     }
 
     void GenerateNewVertices() {
-        foreach(Vector3 loc in testPoints) {
-            for(int i = 0; i < this.numberOfVertices; i++) {
-                Vector3 newVertex = loc;
+        if (this.playerLocations.Count == 1) this.meshVertiecesList.Add(this.playerLocations[0]);
+        for (int i = 0; i < this.numberOfVertices; i++) {
+            Vector3 newVertex = this.playerLocations[this.playerLocations.Count - 1];
 
-                newVertex += Mathf.Sin(this.piValue * i) * gameObject.transform.up;
-                newVertex += Mathf.Cos(this.piValue * i) * gameObject.transform.right;
-                meshVertiecesList.Add(newVertex);
-            }
+            newVertex += Mathf.Sin(this.piValue * i) * gameObject.transform.up;
+            newVertex += Mathf.Cos(this.piValue * i) * gameObject.transform.right;
+            meshVertiecesList.Add(newVertex);
         }
-        this.meshVertiecesList.Add(this.testPoints[this.testPoints.Length - 1]);
     }
 
     void GenerateTriangles() {
-        this.GenerateFace(true);
-        for(int i = 0; i < this.testPoints.Length - 1; i++) {
-            this.GenerateSide(i * this.numberOfVertices + 1);
+        if (this.playerLocations.Count == 1) {
+            this.GenerateFace(true);
+            return;
         }
-        this.GenerateFace(false);
+        this.GenerateSide((this.playerLocations.Count - 2) * this.numberOfVertices + 1);
+        //this.GenerateFace(false);
     }
     void GenerateSide(int startIndex) {
         Debug.Log("side is being generated with start index of: " + startIndex);
-        for(int i = 0; i < this.numberOfVertices; i += 2) {
+        for (int i = 0; i < this.numberOfVertices; i += 2) {
             int v1 = startIndex + i;
             int v2 = startIndex + i + this.numberOfVertices;
             int v3 = startIndex + i + 1;
             int v4 = startIndex + i + 1 + this.numberOfVertices;
             int v5 = i != this.numberOfVertices - 2 ? startIndex + i + 2 : startIndex;
-            int v6 = i != this.numberOfVertices - 2 ? startIndex + i + 2 + this.numberOfVertices: startIndex + this.numberOfVertices;
+            int v6 = i != this.numberOfVertices - 2 ? startIndex + i + 2 + this.numberOfVertices : startIndex + this.numberOfVertices;
 
 
             this.meshTriangles.Add(v1);
@@ -107,18 +109,18 @@ public class MeshSpawningScript : MonoBehaviour {
     }
 
     void GenerateFace(bool first) {
-        if(first) {
+        if (first) {
             for (int i = 0; i < this.numberOfVertices; i++) {
                 this.meshTriangles.Add(1 + i);
                 this.meshTriangles.Add(0);
-                this.meshTriangles.Add(i == this.numberOfVertices - 1 ? 1 :  i + 2);
+                this.meshTriangles.Add(i == this.numberOfVertices - 1 ? 1 : i + 2);
             }
             return;
         }
         for (int i = 0; i < this.numberOfVertices; i++) {
-            this.meshTriangles.Add(i == this.numberOfVertices - 1 ? this.numberOfVertices * (this.testPoints.Length - 1) + 1 : this.numberOfVertices * (this.testPoints.Length - 1) + 2 + i);
+            this.meshTriangles.Add(i == this.numberOfVertices - 1 ? this.numberOfVertices * (this.playerLocations.Count - 1) + 1 : this.numberOfVertices * (this.playerLocations.Count - 1) + 2 + i);
             this.meshTriangles.Add(this.meshVertiecesList.Count - 1);
-            this.meshTriangles.Add(this.numberOfVertices * (this.testPoints.Length - 1) + 1 + i);
+            this.meshTriangles.Add(this.numberOfVertices * (this.playerLocations.Count - 1) + 1 + i);
         }
     }
 }
