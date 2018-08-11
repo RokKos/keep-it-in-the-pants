44391c8cba225993c5a68586adff15e96ee38b2a
Mark Marincek <mark.marincek@gmail.com>
2018-08-12 00:03:09 +0200

    Work in progress on gyroscope

--- keep-it-in-the-pants/Assets/Scripts/GameManager.cs
+++ keep-it-in-the-pants/Assets/Scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour {
 	[SerializeField] private Camera topCamera;
 	[SerializeField] private Camera dickCamera;
 
+<<<<<<< Updated upstream
 	public float dickRadius;
 	public float positionSendingInterval;
 	public Material skinColor;
@@ -24,4 +25,19 @@ public class GameManager : MonoBehaviour {
 		topCamera.enabled = !isDickActive;
 		dickCamera.enabled = isDickActive;
 	}
+=======
+    public float dickRadius;
+    public float positionSendingInterval;
+    public Material skinColor;
+    public bool lockLandscape = false;
+
+    private void Awake() {
+        if (!Instance) {
+            Instance = this;
+        }
+        if(lockLandscape) {
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+        }
+    }
+>>>>>>> Stashed changes
 }
