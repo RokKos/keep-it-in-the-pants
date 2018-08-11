1b7200ef490ce848b0ef3688356045c42628689d
Andrej <andrej.miscic@gmail.com>
2018-08-12 00:46:33 +0200

    Revert "Conflict resolved...."
    This reverts commit 15a50d6170df7071bcb83cb192a000dc2d415590.

--- keep-it-in-the-pants/Assets/Scripts/GameManager.cs
+++ keep-it-in-the-pants/Assets/Scripts/GameManager.cs
@@ -13,23 +13,15 @@ public class GameManager : MonoBehaviour {
 	public float positionSendingInterval;
 	public Material skinColor;
 
-    public bool lockLandscape = false;
 
+	private void Awake () {
+		if (!Instance) {
+			Instance = this;
+		}
+	}
 
-    private void Awake() {
-        if (!Instance) {
-            Instance = this;
-        }
-        if (lockLandscape) {
-            Screen.orientation = ScreenOrientation.LandscapeLeft;
-        }
-    }
-
-    public void ChangeCameras (bool isDickActive) {
+	public void ChangeCameras (bool isDickActive) {
 		topCamera.enabled = !isDickActive;
 		dickCamera.enabled = isDickActive;
 	}
-
-
-
 }
