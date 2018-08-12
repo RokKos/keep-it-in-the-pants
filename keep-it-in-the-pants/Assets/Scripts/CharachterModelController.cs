25755eeb9afd637a4e8b20e2a8ab9453b486e506
Rok Kos <rocki5555@gmail.com>
2018-08-12 09:06:21 +0200

    Forgot to push assets

---
+++ keep-it-in-the-pants/Assets/Scripts/CharachterModelController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharachterModelController : MonoBehaviour {
+
+	[SerializeField] private GameManager gameManager;
+	[SerializeField] private MeshRenderer bodyMesh;
+
+	// Use this for initialization
+	void Start () {
+		bodyMesh.material = gameManager.skinColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ keep-it-in-the-pants/Assets/Scripts/Condom.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Condom : MonoBehaviour {
+
+	[SerializeField] private SpawningController spawningController;
+
+
+	private void OnCollisionEnter (Collision collision) {
+		spawningController.RepositionCondom();
+	}
+}
