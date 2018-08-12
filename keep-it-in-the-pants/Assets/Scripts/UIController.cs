15462867178ccf0bcccde970fc8cc6cbc8e042d4
Rok Kos <rocki5555@gmail.com>
2018-08-12 10:09:12 +0200

    MainUI Scene and getting difficulty

---
+++ keep-it-in-the-pants/Assets/Scripts/UIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+	[SerializeField] private Slider racistSlider;
+
+
+	public void OnPlayGame () {
+		int newValue = (int)racistSlider.value;
+
+		Debug.Log("Racist Difficulty changed from: "+ PlayerPrefs.GetInt("racistDifficulty", -1).ToString() + " to: " + newValue.ToString());
+		PlayerPrefs.SetInt("racistDifficulty", (int)newValue);
+
+		SceneManager.LoadSceneAsync("SpawningScene");
+	}
+}
